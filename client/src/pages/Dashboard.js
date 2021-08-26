b6227e97441708c9ef5b80068ab83a4cb13cfb7a
Fatih Çınar <fcinar_38@hotmail.com>
2021-08-26 13:57:18 +0300

    Edited home page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,12 @@
 import { Helmet } from 'react-helmet';
 import {
   Box,
+  Card,
+  CardHeader,
   Container,
+  Divider,
   Grid
 } from '@material-ui/core';
-import Budget from '../components/dashboard/Budget';
-import TotalCustomers from '../components/dashboard/TotalCustomers';
-import TasksProgress from '../components/dashboard/TasksProgress';
-import TotalProfit from '../components/dashboard/TotalProfit';
-import Sales from '../components/dashboard/Sales';
-import TrafficByDevice from '../components/dashboard/TrafficByDevice';
-import LatestProducts from '../components/dashboard/LatestProducts';
-import LatestOrders from '../components/dashboard/LatestOrders';
 
 const Dashboard = () => (
   <>
@@ -26,83 +21,14 @@ const Dashboard = () => (
       }}
     >
       <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <Budget />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TasksProgress />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalProfit sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <TrafficByDevice sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <LatestProducts sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
-          </Grid>
-        </Grid>
+        <Box sx={{ pt: 3 }}>
+          <Card>
+            <CardHeader
+              title="Duyurular"
+            />
+            <Divider />
+          </Card>
+        </Box>
       </Container>
     </Box>
   </>
