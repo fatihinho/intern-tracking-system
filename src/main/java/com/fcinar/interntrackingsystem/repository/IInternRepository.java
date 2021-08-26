4bd94650dce4af2f3bfd7472cb88ce73a0ffefda
Fatih Çınar <fcinar_38@hotmail.com>
2021-08-26 13:59:08 +0300

    Added find all interns by institutionId

--- src/main/java/com/fcinar/interntrackingsystem/repository/IInternRepository.java
+++ src/main/java/com/fcinar/interntrackingsystem/repository/IInternRepository.java
@@ -4,8 +4,10 @@ import com.fcinar.interntrackingsystem.model.Intern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IInternRepository extends JpaRepository<Intern, UUID> {
+    List<Intern> findAllByInstitutionId(UUID institutionId);
 }
